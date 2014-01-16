b7a418a493105f12d2e53419ec7d5d3192df6613
Peter Chen <pchen@etsy.com>
2014-01-16 14:16:05 -0500

    add empty project

---
+++ SampleIntroScrollAnimation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SampleIntroScrollAnimation
+//
+//  Created by Peter Chen on 1/16/14.
+//  Copyright (c) 2014 Peter Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
