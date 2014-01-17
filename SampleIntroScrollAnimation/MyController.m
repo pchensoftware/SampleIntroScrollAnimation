771222fc3992a1b4a8cf6f9a7cd81460c792de0f
Peter Chen <pchen@etsy.com>
2014-01-17 16:20:37 -0500

    add angle animation

--- SampleIntroScrollAnimation/MyController.m
+++ SampleIntroScrollAnimation/MyController.m
@@ -9,7 +9,7 @@
 #import "MyController.h"
 #import "IFTTTJazzHands.h"
 
-#define kNumPages 2
+#define kNumPages 3
 
 @interface MyController () <UIScrollViewDelegate>
 
@@ -21,6 +21,7 @@
 @property (nonatomic, strong) UIView *page2View2;
 @property (nonatomic, strong) UIView *page2View3;
 @property (nonatomic, strong) IFTTTAnimator *animator;
+@property (nonatomic, readonly) int pageWidth; // equals self.scrollView.frame.size.width, to save space
 
 @end
 
@@ -55,8 +56,13 @@
     self.page2View2 = [self _addViewAtFrame:CGRectMake(self.view.frame.size.width + 150, 200, 30, 30) color:[UIColor greenColor]];
     self.page2View3 = [self _addViewAtFrame:CGRectMake(self.view.frame.size.width + 200, 200, 30, 30) color:[UIColor blueColor]];
     
-    [self _createPage1Animations];
-    [self _createPage2Animations];
+    [self _createPage1To2AnimationsPart1];
+    [self _createPage1To2AnimationsPart2];
+    [self _createPage2To3Animations];
+}
+
+- (int)pageWidth {
+    return self.scrollView.frame.size.width;
 }
 
 - (UIView *)_addViewAtFrame:(CGRect)frame color:(UIColor *)color {
@@ -66,34 +72,47 @@
     return view;
 }
 
-- (void)_createPage1Animations {
-    IFTTTFrameAnimation *page1View1Animation = [IFTTTFrameAnimation animationWithView:self.page1View1];
-    IFTTTFrameAnimation *page1View2Animation = [IFTTTFrameAnimation animationWithView:self.page1View2];
-    IFTTTFrameAnimation *page1View3Animation = [IFTTTFrameAnimation animationWithView:self.page1View3];
-    [self.animator addAnimation:page1View1Animation];
-    [self.animator addAnimation:page1View2Animation];
-    [self.animator addAnimation:page1View3Animation];
-    [page1View1Animation addKeyFrame:[[IFTTTAnimationKeyFrame alloc] initWithTime:20 andFrame:self.page1View1.frame]];
-    [page1View1Animation addKeyFrame:[[IFTTTAnimationKeyFrame alloc] initWithTime:150 andFrame:CGRectMake(150, 100, 75, 75)]];
-    [page1View2Animation addKeyFrame:[[IFTTTAnimationKeyFrame alloc] initWithTime:50 andFrame:self.page1View2.frame]];
-    [page1View2Animation addKeyFrame:[[IFTTTAnimationKeyFrame alloc] initWithTime:150 andFrame:CGRectMake(250, 100, 75, 75)]];
-    [page1View3Animation addKeyFrame:[[IFTTTAnimationKeyFrame alloc] initWithTime:80 andFrame:self.page1View3.frame]];
-    [page1View3Animation addKeyFrame:[[IFTTTAnimationKeyFrame alloc] initWithTime:150 andFrame:CGRectMake(350, 100, 75, 75)]];
+- (void)_createPage1To2AnimationsPart1 {
+    IFTTTFrameAnimation *view1Animation = [IFTTTFrameAnimation animationWithView:self.page1View1];
+    IFTTTFrameAnimation *view2Animation = [IFTTTFrameAnimation animationWithView:self.page1View2];
+    IFTTTFrameAnimation *view3Animation = [IFTTTFrameAnimation animationWithView:self.page1View3];
+    [view1Animation addKeyFrame:[[IFTTTAnimationKeyFrame alloc] initWithTime:20 andFrame:self.page1View1.frame]];
+    [view1Animation addKeyFrame:[[IFTTTAnimationKeyFrame alloc] initWithTime:150 andFrame:CGRectMake(150, 100, 75, 75)]];
+    [view2Animation addKeyFrame:[[IFTTTAnimationKeyFrame alloc] initWithTime:50 andFrame:self.page1View2.frame]];
+    [view2Animation addKeyFrame:[[IFTTTAnimationKeyFrame alloc] initWithTime:150 andFrame:CGRectMake(250, 100, 75, 75)]];
+    [view3Animation addKeyFrame:[[IFTTTAnimationKeyFrame alloc] initWithTime:80 andFrame:self.page1View3.frame]];
+    [view3Animation addKeyFrame:[[IFTTTAnimationKeyFrame alloc] initWithTime:150 andFrame:CGRectMake(350, 100, 75, 75)]];
+    [self.animator addAnimation:view1Animation];
+    [self.animator addAnimation:view2Animation];
+    [self.animator addAnimation:view3Animation];
 }
 
-- (void)_createPage2Animations {
-    IFTTTFrameAnimation *page2View1Animation = [IFTTTFrameAnimation animationWithView:self.page2View1];
-    IFTTTFrameAnimation *page2View2Animation = [IFTTTFrameAnimation animationWithView:self.page2View2];
-    IFTTTFrameAnimation *page2View3Animation = [IFTTTFrameAnimation animationWithView:self.page2View3];
-    [self.animator addAnimation:page2View1Animation];
-    [self.animator addAnimation:page2View2Animation];
-    [self.animator addAnimation:page2View3Animation];
-    [page2View1Animation addKeyFrame:[[IFTTTAnimationKeyFrame alloc] initWithTime:200 andFrame:self.page2View1.frame]];
-    [page2View1Animation addKeyFrame:[[IFTTTAnimationKeyFrame alloc] initWithTime:320 andFrame:CGRectMake(self.scrollView.frame.size.width + 50, 300, 100, 100)]];
-    [page2View2Animation addKeyFrame:[[IFTTTAnimationKeyFrame alloc] initWithTime:210 andFrame:self.page2View2.frame]];
-    [page2View2Animation addKeyFrame:[[IFTTTAnimationKeyFrame alloc] initWithTime:320 andFrame:CGRectMake(self.scrollView.frame.size.width + 100, 350, 100, 100)]];
-    [page2View3Animation addKeyFrame:[[IFTTTAnimationKeyFrame alloc] initWithTime:220 andFrame:self.page2View3.frame]];
-    [page2View3Animation addKeyFrame:[[IFTTTAnimationKeyFrame alloc] initWithTime:320 andFrame:CGRectMake(self.scrollView.frame.size.width + 150, 400, 100, 100)]];
+- (void)_createPage1To2AnimationsPart2 {
+    IFTTTFrameAnimation *view1Animation = [IFTTTFrameAnimation animationWithView:self.page2View1];
+    IFTTTFrameAnimation *view2Animation = [IFTTTFrameAnimation animationWithView:self.page2View2];
+    IFTTTFrameAnimation *view3Animation = [IFTTTFrameAnimation animationWithView:self.page2View3];
+    [view1Animation addKeyFrame:[[IFTTTAnimationKeyFrame alloc] initWithTime:200 andFrame:self.page2View1.frame]];
+    [view1Animation addKeyFrame:[[IFTTTAnimationKeyFrame alloc] initWithTime:320 andFrame:CGRectMake(self.pageWidth + 50, 300, 100, 100)]];
+    [view2Animation addKeyFrame:[[IFTTTAnimationKeyFrame alloc] initWithTime:210 andFrame:self.page2View2.frame]];
+    [view2Animation addKeyFrame:[[IFTTTAnimationKeyFrame alloc] initWithTime:320 andFrame:CGRectMake(self.pageWidth + 100, 350, 100, 100)]];
+    [view3Animation addKeyFrame:[[IFTTTAnimationKeyFrame alloc] initWithTime:220 andFrame:self.page2View3.frame]];
+    [view3Animation addKeyFrame:[[IFTTTAnimationKeyFrame alloc] initWithTime:320 andFrame:CGRectMake(self.pageWidth + 150, 400, 100, 100)]];
+    [self.animator addAnimation:view1Animation];
+    [self.animator addAnimation:view2Animation];
+    [self.animator addAnimation:view3Animation];
+}
+
+- (void)_createPage2To3Animations {
+    IFTTTFrameAnimation *stayView1Animation = [IFTTTFrameAnimation animationWithView:self.page2View2];
+    [stayView1Animation addKeyFrame:[[IFTTTAnimationKeyFrame alloc] initWithTime:200 andFrame:self.page2View2.frame]];
+    [stayView1Animation addKeyFrame:[[IFTTTAnimationKeyFrame alloc] initWithTime:1*self.pageWidth andFrame:CGRectMake(1*self.pageWidth + 100, 350, 100, 100)]];
+    [stayView1Animation addKeyFrame:[[IFTTTAnimationKeyFrame alloc] initWithTime:2*self.pageWidth andFrame:CGRectMake(2*self.pageWidth + 100, 350, 100, 100)]];
+    [self.animator addAnimation:stayView1Animation];
+    
+    IFTTTAngleAnimation *rotateView1Animation = [IFTTTAngleAnimation animationWithView:self.page2View2];
+    [rotateView1Animation addKeyFrame:[[IFTTTAnimationKeyFrame alloc] initWithTime:1*self.pageWidth andAngle:0]];
+    [rotateView1Animation addKeyFrame:[[IFTTTAnimationKeyFrame alloc] initWithTime:2*self.pageWidth andAngle:2*M_PI]];
+    [self.animator addAnimation:rotateView1Animation];
 }
 
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView {
